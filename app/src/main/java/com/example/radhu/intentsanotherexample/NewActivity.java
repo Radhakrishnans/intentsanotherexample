c65307273dff121ca1aba3ac3bcda9347e7786da
Radhu Punchanathan <radhakrishnan.punchanathan@rmit.edu.au>
2020-07-26 21:13:14 +1000

    Commited

---
+++ app/src/main/java/com/example/radhu/intentsanotherexample/NewActivity.java
@@ -0,0 +1,14 @@
+package com.example.radhu.intentsanotherexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_activity);
+    }
+}
